e6489a0576dc150a072894889186ad1b9b8ee401
Mateusz Izydorczyk <tionzee@gmail.com>
2022-11-16 17:25:50 +0100

    Docs

--- TasksAPI/Controllers/GroupController.cs
+++ TasksAPI/Controllers/GroupController.cs
@@ -24,7 +24,7 @@ namespace TasksAPI.Controllers
             return Created($"/api/group/{id}", null);
         }
 
-        [HttpPost("join ")]
+        [HttpPost("join")]
         public ActionResult Join([FromHeader]string invitationCode)
         {
             _groupService.Join(invitationCode);
